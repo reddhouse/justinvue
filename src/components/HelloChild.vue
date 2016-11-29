e441c6512e828da4b87f4c19b138a8fabf637b0f
Justin Tuttle <reddhouse@gmail.com>
2016-11-28 19:41:27 -0800

    Fix punctuation error

---
+++ src/components/HelloChild.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello-child-component">
+    <h1>Testing HelloChild Component</h1>
+    {{ msg }}
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'hello-child-component',
+  data () {
+    return {
+      msg: 'This message is inside of HelloChild.vue'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
